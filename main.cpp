ba2c2a3e4a7b09e54d807961e3f7bec183a13cb5
Rhee <krhee0804@snu.ac.kr>
2024-06-07 19:42:54 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,13 +7,18 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+	if (vec.size() == M) {
+		for (int i = 0; i < M; i++) {
+			cout << vec[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+	for (int i = cnt; i < N; i++) {
+		vec.push_back(i + 1);
+		func(i + 1);
+		vec.pop_back();
+	}
 }
 
 int main() {
